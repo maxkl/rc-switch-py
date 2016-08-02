a321ea205b62839d61f33cbd03f18e580f245f50
Max Klein <max.kleinleibold@gmail.com>
2016-08-02 18:08:20 +0200

    Move setup function into RCSwitch class

--- rcswitch.py
+++ rcswitch.py
@@ -8,10 +8,12 @@ lib.RCSwitch_RCSwitch.restype = c_void_p
 lib.RCSwitch_switchOn.argtypes = [c_void_p, c_char_p, c_char_p]
 lib.RCSwitch_switchOff.argtypes = [c_void_p, c_char_p, c_char_p]
 
-def setup():
-	lib.setup()
 
 class RCSwitch:
+	@staticmethod
+	def setup():
+		lib.setup()
+
 	def __init__(self):
 		self.obj = lib.RCSwitch_RCSwitch()
 	
@@ -26,4 +28,3 @@ class RCSwitch:
 
 	def switch_off(self, group, device):
 		lib.RCSwitch_switchOff(self.obj, group.encode('utf-8'), device.encode('utf-8'))
-
